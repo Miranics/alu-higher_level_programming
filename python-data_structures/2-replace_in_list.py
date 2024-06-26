0f6bc405340d832c6958c51beb1edde4ce12455d
Miranics <002nasya@gmail.com>
2024-06-26 02:37:27 -0700

    sorting task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,7 +2,6 @@
 
 def replace_in_list(my_list, idx, element):
     """function that replaces an element at specific position
-
     If idx is negative, the function should not modify anything, and
     returns the original list
     If idx is out of range (> of number of element in my_list), the
@@ -10,7 +9,6 @@ def replace_in_list(my_list, idx, element):
     You are not allowed to import any module
     You are not allowed to use try/except
     """
-
     if idx < 0:
         return my_list
     elif idx > len(my_list) - 1:
