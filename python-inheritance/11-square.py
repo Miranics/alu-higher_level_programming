47daa01f9e8156d2afe183cf66cdc63dbf95c335
Miranics <002nasya@gmail.com>
2024-07-12 09:52:04 -0700

    task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-Created on Fri May 29 15:13:37 2020
-
-@author: Robinson Montes
+Module 10-square
+Contains parent class BaseGeometry
+with public instance method area and integer_validation
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent,
+extends parent's area method and prints with __str__
+Contains subclass Square
+with instantiation of private attribute size, validated by superclass,
+and prints with __str__
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(BaseGeometry):
-    """
-    A Square class shape, inheirts from BaseGeometry
+class Square(Rectangle):
+    """inherits from Rectangle, who inherits from BaseGeometry
+    methods:
+        __init__(self, size)
+        __str__(self)
     """
     def __init__(self, size):
-        """"
-        Init function for Square
-
-        Attributes:
-            size (int): The size of the square
+        """initializes size
+        Args:
+            size (int): private
         """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
-        """
-        str funtion to print with/height
-
-        Returns:
-            Return width/height
-        """
-        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
-
-    def area(self):
-        """
-        A function that calculates the area of the Square
-        """
-        return self.__size ** 2
+        """prints [Rectangle] <width>/<height>"""
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__size, self.__size)
