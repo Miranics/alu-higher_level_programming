cd50c468091dedba30ebb023c26fa7798a728ad0
miranics <002nasya@gmail.com>
2024-07-17 17:37:21 +0200

    'files 6-12'

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -21,15 +21,12 @@ def pascal_triangle(n):
         return []
     if n == 1:
         return [[1]]
-
     triangle = [[1]]
     for rows in range(n-1):
         triangle.append([a+b for a, b
                          in zip([0] + triangle[-1], triangle[-1] + [0])])
     return triangle
-
 # C implementation of the last Pythonic loop
-#
 #    triangle = [[1]]
 #    for rows in range(n-1):
 #        l = [1]
