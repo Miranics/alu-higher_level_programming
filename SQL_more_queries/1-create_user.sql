4151f5cc1a5feaf044283108246d06d2bce6ed14
Miranics <002nasya@gmail.com>
2024-07-19 09:34:13 -0700

    task 0

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,10 @@
 -- Script to create MySQL user user_0d_1 with all privileges and set password
 
 -- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user on all databases and tables
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Apply the changes
 FLUSH PRIVILEGES;
