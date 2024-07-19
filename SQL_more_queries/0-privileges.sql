05595fd37f37dad9e35d762a91bc6b06c7940f01
Miranics <002nasya@gmail.com>
2024-07-19 09:43:21 -0700

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,7 @@
--- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- Script to list all privileges of MySQL users user_0d_1 and user_0d_2 on the server
 
+-- List all privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
+-- List all privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
