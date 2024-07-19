6a9e558721b3c1517731ac026a2f250a669dbb29
Miranics <002nasya@gmail.com>
2024-07-19 05:23:26 -0700

    task0-1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
---Create the database hbtn_0c_0 if it does not already exist 
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+-- Create the database hbtn_0c_0 if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
