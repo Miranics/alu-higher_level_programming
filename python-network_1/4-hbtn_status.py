df217f041f69f4425b65216f30af43149245a26c
miranics <002nasya@gmail.com>
2024-08-01 11:23:41 +0200

    Fetching

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-Fetch http://0.0.0.0:5050/status and display the response.
+Fetch https://intranet.hbtn.io/status and display the response.
 
-This module uses the requests library to send an HTTP GET request to the specified URL,
-retrieves the response, and prints the details about the response body, including its type
-and content.
+This module uses the requests library to send an HTTP GET request to the
+specified URL, retrieves the response, and prints the details about the
+response body, including its type and content.
 """
 
 import requests
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
+    url = "https://intranet.hbtn.io/status"
     response = requests.get(url)
     content = response.text
 
