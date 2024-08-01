fc6107af31478776541b614dc3d3b86bb5dad1fd
miranics <002nasya@gmail.com>
2024-08-01 09:24:03 +0200

    Fetching

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+"""
+This module fetches the status from https://alu-intranet.hbtn.io/status
+using the requests package and displays the response body details.
+"""
 
+import requests
+
+def fetch_status():
+    """Fetches the status from the specified URL and prints the response details."""
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+    content = response.text
+
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
-        
+    fetch_status()
