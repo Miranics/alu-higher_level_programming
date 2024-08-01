16d9c226c870f3357b86edb2a6ddd910effae55d
miranics <002nasya@gmail.com>
2024-08-01 09:03:13 +0200

    Fetching

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,14 +2,18 @@
 
 import urllib.request
 
+# The URL to fetch the status from
 url = 'https://alu-intranet.hbtn.io/status'
 
+# Open the URL and fetch the response using a with statement
 with urllib.request.urlopen(url) as response:
+    # Read the response body as bytes
     body = response.read()
+    # Decode the body to a UTF-8 string
     utf8_content = body.decode('utf-8')
 
+# Display the response body information
 print("Body response:")
 print(f"\t- type: {type(body)}")
 print(f"\t- content: {body}")
 print(f"\t- utf8 content: {utf8_content}")
-
