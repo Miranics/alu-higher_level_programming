cf5c994d72b7660fe0f2c9cf1382b7caec5d9b2b
miranics <002nasya@gmail.com>
2024-08-05 17:11:43 +0200

    if url.startswith('https://'):

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""
-
-This module is composed by a function prints a message
-
-"""
+"""Module for say_my_name method."""
 
 
 def say_my_name(first_name, last_name=""):
-    """ Function that prints "My name is <first name> <last name>"
+    """Method for printing first and last name.
 
     Args:
-        first_name: first name
-        last_name: last name
-
-    Returns:
-        No return
+        first_name: first name string.
+        last_name: last name string.
 
     Raises:
-        TypeError: If first_name or last_name is not a string
-
-
+        TypeError: If first_name or last_name are not strings.
     """
-
-    if type(first_name) is not str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
-    if type(last_name) is not str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
