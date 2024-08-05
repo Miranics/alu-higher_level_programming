58ce7e503fb020cbe0cd10fad6378c436e2b26e9
miranics <002nasya@gmail.com>
2024-08-05 15:27:12 +0200

    last name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """
-
-This module is composed by a function prints a message
-
+This module is composed of a function that prints a message
 """
-
 
 def say_my_name(first_name, last_name=""):
     """ Function that prints "My name is <first name> <last name>"
@@ -18,8 +15,6 @@ def say_my_name(first_name, last_name=""):
 
     Raises:
         TypeError: If first_name or last_name is not a string
-
-
     """
 
     if type(first_name) is not str:
@@ -28,4 +23,7 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
