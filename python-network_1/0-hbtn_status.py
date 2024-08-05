6c91c1496944b4496463f52d7a775b385811b33a
miranics <002nasya@gmail.com>
2024-08-05 16:41:00 +0200

    /

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python3
-"""
-This module defines a function to fetch and display the status of a web page.
-
-The function fetch_status sends an HTTP GET request to the URL 'https://alu-intranet.hbtn.io/status'
-using the urllib package, retrieves the response, and prints the details of the response body.
-The details include the type of the response body, the content as bytes, and the UTF-8 decoded content.
-
-Usage:
-    This script should be run directly to fetch and display the status from the specified URL.
-"""
-
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
 
-def fetch_status():
-    """Fetches the status from the specified URL and prints the response details."""
-    url = 'https://alu-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        utf8_content = body.decode('utf-8')
-
-    print("Body response:")
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
-    print(f"\t- utf8 content: {utf8_content}")
-
-
 if __name__ == "__main__":
-    fetch_status()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
