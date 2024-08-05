1f61900383fb7464b52d2d6441215f0b9e036356
miranics <002nasya@gmail.com>
2024-08-05 16:43:06 +0200

    /

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 """
-Fetch https://intranet.hbtn.io/status and display the response.
-
-This script uses the requests library to perform an HTTP GET request
-to the specified URL, and prints the response details, including the
-type and content of the response body.
+Python script that fetches an URL with requests package
 """
-
 import requests
 
-if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
-    content = response.text
 
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
