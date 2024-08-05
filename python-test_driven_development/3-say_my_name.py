04afb99d972e354876f9dd8fc7432ed55d9f5b7c
miranics <002nasya@gmail.com>
2024-08-05 17:20:04 +0200

    Traceback

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""Module for say_my_name method."""
+# Function that prints My name is <first name> <last name>
+"""
+    Define 'say_my_name' function.
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Method for printing first and last name.
-
-    Args:
-        first_name: first name string.
-        last_name: last name string.
-
-    Raises:
-        TypeError: If first_name or last_name are not strings.
+    """
+        Print a name.
+        Args:
+            first_name (str): first name to print.
+            last_name (str): last name to print.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print("My name is {:s} {:s}".format(first_name, last_name))
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/3-say_my_name.txt")
+    print("My name is {} {}".format(first_name, last_name))
