e94fd4aacf54203ae2fdc71671e17d3c8a37338d
miranics <002nasya@gmail.com>
2024-08-05 17:24:46 +0200

    Traceback

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-# Function that prints My name is <first name> <last name>
-"""
-    Define 'say_my_name' function.
-"""
+"""A function that prints the user's first name and last name"""
 
 
 def say_my_name(first_name, last_name=""):
     """
-        Print a name.
-        Args:
-            first_name (str): first name to print.
-            last_name (str): last name to print.
+    Function that prints "My name is <first name> <last name>"
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
