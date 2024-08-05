945440f4a6a058f256452231ac296fea3ff0aa70
miranics <002nasya@gmail.com>
2024-08-05 16:28:07 +0200

    trailing space

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-This module is composed of a function that prints a message
+
+This module is composed by a function prints a message
+
 """
 
 
@@ -16,12 +18,14 @@ def say_my_name(first_name, last_name=""):
 
     Raises:
         TypeError: If first_name or last_name is not a string
+
+
     """
 
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
 
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}")
+    print("My name is {} {}".format(first_name, last_name))
