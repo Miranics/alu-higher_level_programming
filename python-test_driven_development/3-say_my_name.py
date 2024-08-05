e175506afc4dfb4204e7548d4d472d6e3eb45803
miranics <002nasya@gmail.com>
2024-08-05 15:53:39 +0200

    last name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -25,6 +25,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     if last_name:
-        print(f"My name is {first_name} {last_name}")
+        print(f"My name is {first_name} {last_name} ")
     else:
-        print(f"My name is {first_name}")
+        print(f"My name is {first_name} ")
