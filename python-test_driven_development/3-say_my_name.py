c4dceb9ec7112f9e48e6f6be7f83dc45883c4fac
miranics <002nasya@gmail.com>
2024-08-05 16:19:59 +0200

    trailing space

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -24,7 +24,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    if last_name:
-        print(f"My name is {first_name} {last_name}\n")
-    else:
-        print(f"My name is {first_name}\n")
+    print(f"My name is {first_name} {last_name}")
